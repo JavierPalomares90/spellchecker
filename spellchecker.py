cdb2ef96fc76ebd6cacbe2dda3f334859ad69ccf
Javier Palomares <javier.palomares.90@gmail.com>
2019-10-01 17:59:00 -0500

    Initial commit

---
+++ spellchecker.py
@@ -0,0 +1,16 @@
+#
+import argparse
+
+
+def parse_args():
+    pass
+
+def read_master_list():
+    pass
+
+def main():
+    parse_args()
+    read_master_list()
+
+if __name__ == '__main__':
+    main()
