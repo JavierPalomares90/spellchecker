20bd72bcd07ac4321ddd48a599c72aeea39c4e53
Javier Palomares <javier.palomares.90@gmail.com>
2020-01-04 12:36:42 -0600

    Move into new packages

--- fuzzy/spellchecker.py
+++ fuzzy/spellchecker.py
@@ -30,12 +30,12 @@ def main():
     while True:
         query = input("What are you looking for?\n").lower()
         # Compute the best choice using the leveshtein distance and WRatio
-        print("Using token set ratio")
-        print(process.extract(query,choices,scorer=fuzz.token_set_ratio))
-        print("Using partial token set ratio")
-        print(process.extract(query,choices,scorer=fuzz.partial_token_set_ratio))
-        print("Using partial ratio")
-        print(process.extract(query,choices,scorer=fuzz.partial_ratio))
+        #print("Using token set ratio")
+        #print(process.extract(query,choices,scorer=fuzz.token_set_ratio))
+        #print("Using partial token set ratio")
+        #print(process.extract(query,choices,scorer=fuzz.partial_token_set_ratio))
+        #print("Using partial ratio")
+        #print(process.extract(query,choices,scorer=fuzz.partial_ratio))
         best = process.extractOne(query,choices, scorer=fuzz.token_sort_ratio)
         print("I think you meant: {}".format(best))
 
