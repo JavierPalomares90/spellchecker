9414bf797c860ff7ccca40b746cf5417e3ac9427
Javier Palomares <javier.palomares.90@gmail.com>
2020-02-01 15:45:00 -0600

    More partial impl

--- symspell/compound/SymspellCompound.py
+++ symspell/compound/SymspellCompound.py
@@ -3,9 +3,13 @@ from symspell.SymspellDictionary import SymspellDictionary
 from symspell.SymspellVerbosity import SymspellVerbosity
 from symspell.SymspellSuggestion import SymspellSuggestion
 import re
+import sys
 
 class SymspellCompound(SymspellDictionary):
     def __init__(self, count_threshold=1, max_dictionary_edit_distance=2, prefix_len=7):
+        #TODO: Populate bigram
+        self.bigrams = dict()
+        self.bigram_count_min = sys.maxsize
         super().__init__(count_threshold=count_threshold, max_dictionary_edit_distance=max_dictionary_edit_distance, prefix_len=prefix_len)
 
     def lookup_compound(input):
@@ -79,7 +83,50 @@ class SymspellCompound(SymspellDictionary):
                             
                             if len(suggestions2) > 0:
                                 # select best suggestion for split pair
-                                # TODO: Complete impl
+                                suggestion_split.term = suggestions1[0].term + " " + suggestions2[0].term
+
+                                distance2 = edit_distance.compare(terms[i],suggestion_split.term,max_edit_distance)
+                                if distance2 < 0:
+                                    distance2 = max_edit_distance + 1
+                                
+                                if suggestion_split_best is not None:
+                                    if distance2 > suggestion_split_best.distance:
+                                        continue
+                                    if distance2 < suggestion_split_best.distance:
+                                        suggestion_split_best = None
+
+                                suggestion_split.distance = distance2
+                                # if bigram exists in bigram dictionary
+                                bigram_count = self.bigrams[suggestion_split.term] 
+                                if bigram_count is not None:
+                                    suggestion_split.count = bigram_count
+
+                                    # increaase count if split corrections are part of or idential to input
+
+                                    # single term correction exists
+                                    if len(suggestions) > 0:
+                                        
+                                        # remove the single term from suggestions_split
+                                        if (suggestions1[0].term + suggestions2[0].term == terms[i]):
+                                            # TODO: Check why +2 here
+                                            suggestion_split.count = max(suggestion_split.count,len(suggestions[0]) + 2)
+                                        elif ((suggestions1[0].term == suggestions[0].term ) or suggestion2[0].term == suggestions[0].term):
+                                            # make count bigger than the count of a single term correction
+                                            suggestion_split.count = max(suggestion_split.count,suggestions[0].count + 1)
+                                    elif (suggestions1[0].term + suggestions2[0].term == terms[i]):
+                                        # no single term correction exists
+                                        suggestions_split.count = max(suggestion_split.count, max(suggestions1[0].count,suggestion2[0].count) + 2)
+                                else:
+                                    # The Bayes's prob of the word combination is the product of the 2 word probs
+                                    # P(A B) = P(A) * P(B)
+                                    # use it to estimate the frequency count of the combination which is then used to rank the best splitting variatn
+                                    suggestion_split.count = min(bigra)
+
+
+            
+
+
+
                                 pass
 
 
